fa9c0e075108be5bec20153bb675efbc087f6df7
Gregory Rodriguez <glrd4726ml@gmail.com>
2024-05-16 17:29:08 -0300

    Funcion de carga datos

--- Func/index.js
+++ Func/index.js
@@ -1,34 +1,40 @@
-const agregarAlumnos = () => {
-    var participantes = []
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-
-    participantes.push({ nombre, apellido, email });
-    localStorage.setItem('agregar', JSON.stringify(participantes));
-    grupoForm.reset();
-    console.log(localStorage.agregar);
-
-        //Dibujamos la informacion
-        document.getElementById("alumnos").innerHTML = localStorage.agregar.apellido;
-}
-
-/*
 document.addEventListener("DOMContentLoaded", function() {
     const grupoForm = document.getElementById('grupoForm');
     const crearGrupo = document.getElementById('crearGrupo');
     const grupoVisualizacion = document.getElementById('grupoVisualizacion');
     let participantes = [];
 
-    grupoForm.addEventListener('submit', function(event) {
-        event.onclick();
+})
+
+const agregarAlumnos = () => {
+    var participantes = [];
+
+    var nombre = document.getElementById('nombre').value.trim();
+    var apellido = document.getElementById('apellido').value.trim();
+    var email = document.getElementById('email').value.toLowerCase().trim();
+
+    participantes.push({ nombre, apellido, email });
+    window.localStorage.setItem('Nombre', nombre);
+    window.localStorage.setItem('Apellido', apellido);
+    window.localStorage.setItem('Email', email);
+    
+    grupoForm.reset();    
+    console.log(participantes);
+   
+    document.getElementById("nombreM").innerHTML = localStorage.getItem('Nombre')
+    document.getElementById("apellidoM").innerHTML = localStorage.getItem('Apellido');
+    document.getElementById("emailM").innerHTML = localStorage.getItem('Email')
+}
+    /*grupoForm.addEventListener('submit', function(event) {
+        event.click();
         const nombre = document.getElementById('nombre').value;
+        const apellido = document.getElementById('nombre').value;
         const email = document.getElementById('email').value;
         participantes.push({ nombre, email });
         grupoForm.reset();
     });
 
-    crearGrupo.addEventListener('onclick', function() {
+    crearGrupo.addEventListener('click', function() {
         if (participantes.length === 0) {
             alert('Agregue al menos un usuario antes de crear grupos.');
             return;
@@ -43,11 +49,10 @@ document.addEventListener("DOMContentLoaded", function() {
             
             participantesPorGrupo.forEach(user => {
                 const participanteData = document.createElement('p');
-                participanteData.textContent = `${user.nombre} - ${user.email}`;
+                participanteData.textContent = `${user.nombre} - ${user.apellido} - ${user.email}`;
                 grupo.appendChild(participanteData); 
             });
             grupoVisualizacion.appendChild(grupo);
         }
     });
-});
-*/
+    */
